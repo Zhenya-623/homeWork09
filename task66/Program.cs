b11f146cf01a7909b1022877c910a1b42700b9c9
Евгений Политыко <Zhenya-623@internet.ru>
2022-12-12 14:55:54 +0300

    homeWork is ready

---
+++ task66/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите два числа число:");
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+if (m > n)
+{
+    Console.WriteLine("Число М не может быть больше N");
+}
+else
+{
+    int sum = SumNaturalBeetwenNumbers(n, m);
+    Console.WriteLine(sum);
+}
+
+int SumNaturalBeetwenNumbers(int numN, int numM)
+{
+    if (numN < numM) return 0;
+    int sum = SumNaturalBeetwenNumbers(numN - 1, numM);
+    return sum += numN;
+}
---
+++ task68/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+Console.WriteLine("Введите число M");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+if (numberM >= 0 && numberN >= 0)
+{
+    int sum = Ackerman(numberN, numberM);
+    Console.WriteLine(sum);
+}
+else
+{
+    Console.WriteLine("Число M и N не могут быть отрицательными");
+}
+
+int Ackerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (m > 0 && n == 0)
+    {
+        return Ackerman(m - 1, 1);
+    }
+    else
+    {
+        return Ackerman(m - 1, Ackerman(m, n - 1));
+    }
+}
